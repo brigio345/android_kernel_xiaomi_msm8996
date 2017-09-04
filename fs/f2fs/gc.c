261895c25271259a0c34eca23f7a8e4423f12158
Yunlong Song <yunlong.song@huawei.com>
2017-09-04 11:10:18 +0800

    Revert "f2fs: add a new function get_ssr_cost"
    commit 2afce76a1151fe2f1104962c327d8f85047045e6 upstream.
    
    This reverts commit b7b7c4cf1c9ef0272a65f1480457cbfdadcda19d.
    
    se->ckpt_valid_blocks will never be smaller than se->valid_blocks, so just
    remove get_ssr_cost.
    
    Signed-off-by: Yunlong Song <yunlong.song@huawei.com>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Git-commit: 5a3512a921b820b13416a32561bfc2bc26e963ee
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -267,6 +267,16 @@ static unsigned int get_cb_cost(struct f2fs_sb_info *sbi, unsigned int segno)
 	return UINT_MAX - ((100 * (100 - u) * age) / (100 + u));
 }
 
+static unsigned int get_greedy_cost(struct f2fs_sb_info *sbi,
+						unsigned int segno)
+{
+	unsigned int valid_blocks =
+			get_valid_blocks(sbi, segno, true);
+
+	return IS_DATASEG(get_seg_entry(sbi, segno)->type) ?
+				valid_blocks * 2 : valid_blocks;
+}
+
 static inline unsigned int get_gc_cost(struct f2fs_sb_info *sbi,
 			unsigned int segno, struct victim_sel_policy *p)
 {
